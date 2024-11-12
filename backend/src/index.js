9caa621d0dcd7b46fb0e325170ff2e213b2c09eb
nighthunter1305 <mthanhdat1305@gmail.com>
2024-11-12 18:20:19 +0700

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,6 +14,7 @@ app.use(cors({
   credentials: true
 }));
 
+app.use('/', customerRoutes);
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(cookieParser());
