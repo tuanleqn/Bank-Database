0eaf3a3bc3d01605de8798259a85b5fe55623cc4
Le Duc Anh Tuan <tuanleqn04@gmail.com>
2024-11-16 15:23:20 +0700

    add api/getAccCustommerByName

--- backend/src/routes/AdminRouter.js
+++ backend/src/routes/AdminRouter.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const AdminController = require('../controllers/AdminController');
 
 router.get('/infor_user', AdminController.getInforUser);
-
+router.get('/customer_by_name', AdminController.getCustomerByName);
 
 module.exports = router
